07dc71be88141b5a7ab4b43f4f0bdb0c8d98923a
Ambrevar <ambrevar@gmail.com>
2012-09-02 19:32:47 +0200

    Homeinit: Purged old files. gtkrc: removed include + comments. homeinit: minor fix. Shell: new netspeed function + various fixes.

--- .config/awesome/themes/getskin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-[ -d /usr/share/awesome/themes/default ] && cp -rn "/usr/share/awesome/themes/default/" "${HOME}/.config/awesome/themes/custom"
--- .config/luakit/adblock-list-fetcher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$XDG_DATA_HOME/luakit"
-wget 'https://easylist-downloads.adblockplus.org/easylist.txt' -O "$XDG_DATA_HOME/luakit/easylist.txt"
