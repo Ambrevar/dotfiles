4e86202c5d951490ab3d2bd32995524d6ae0de7e
Ambrevar <ambrevar@gmail.com>
2012-09-27 17:56:08 +0200

    Awesome: renamed tags with unicode symbols. Luakit: added a bookmark converter script.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -86,7 +86,8 @@ tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     -- tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
-    tags[s] = awful.tag({ " 1 ", " 2 "," 3 "," 4 ", " 5. Factory ", " 6. Music ", "7. Mail " }, s, layouts[1])
+    -- tags[s] = awful.tag({ " 1 ", " 2 "," 3 "," 4 ", " 5. Factory ", " 6. Music ", "7. Mail " }, s, layouts[1])
+    tags[s] = awful.tag({ " 1 ", " 2 "," 3 "," 4 ", " 5 ⚒ ", " 6 ♫ ", "7 ✉ " }, s, layouts[1])
 end
 -- }}}
 
