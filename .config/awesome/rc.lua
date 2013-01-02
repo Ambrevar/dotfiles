880efa1cbb063611c2690063f3a1e6ed21cd808c
Ambrevar <ambrevar@gmail.com>
2013-01-02 11:39:25 +0100

    Awesome: option to remove gaps between windows.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -568,6 +568,7 @@ awful.rules.rules = {
    { rule = { },
      properties = { border_width = beautiful.border_width,
                     border_color = beautiful.border_normal,
+                    size_hints_honor = false,
                     focus = true,
                     keys = clientkeys,
                     buttons = clientbuttons } },
