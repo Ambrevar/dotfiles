7e76b3676951fadfdae9ef28dcf5bf3218721ec2
Pierre Neidhardt <ambrevar@gmail.com>
2013-02-06 14:55:32 +0100

    switchdriver fancy output.

--- .switchdriver.sh
+++ .switchdriver.sh
@@ -7,11 +7,13 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 
-if [ "$(pacman -Qi catalyst-utils 2>/dev/null|wc -l)" -ge 2 ]; then
+if [ $(pacman -Qi catalyst-utils 2>/dev/null|wc -l) -ge 2 ]; then
     pacman -Rdd --noconfirm catalyst-utils catalyst-dkms lib32-catalyst-utils
     rm -f "/etc/X11/xorg.conf"
     pacman -S --noconfirm ati-dri lib32-ati-dri  xf86-video-ati 
     sed -i 's/nomodeset//' "/boot/syslinux/syslinux.cfg"
+    echo
+    echo "#### Syslinux entry:"
     grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
 else
     pacman -Rs --noconfirm ati-dri lib32-ati-dri  xf86-video-ati 
@@ -19,5 +21,7 @@ else
     pacman -S  --noconfirm catalyst-utils catalyst-dkms lib32-catalyst-utils
     aticonfig --initial
     sed -i '/^LABEL arch$/{n;n;n;s/$/ nomodeset/}' "/boot/syslinux/syslinux.cfg"
+    echo
+    echo "#### Syslinux entry:"
     grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
 fi
