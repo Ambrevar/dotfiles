c32f3de9f30b3c6e999760a387db1345ce93b415
Pierre Neidhardt <ambrevar@gmail.com>
2013-02-06 15:15:54 +0100

    switchdriver: syslinux safety.

--- .switchdriver.sh
+++ .switchdriver.sh
@@ -16,17 +16,27 @@ if [ $(pacman -Qi catalyst-utils 2>/dev/null|wc -l) -ge 2 ]; then
     pacman -Rdd --noconfirm catalyst-utils catalyst-dkms lib32-catalyst-utils
     rm -f "/etc/X11/xorg.conf"
     pacman -S --noconfirm ati-dri lib32-ati-dri  xf86-video-ati 
-    sed -i 's/nomodeset//' "/boot/syslinux/syslinux.cfg"
-    echo
-    echo "#### Syslinux entry:"
-    grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
+    if [ -f "/boot/syslinux/syslinux.cfg" ]; then
+        sed -i 's/nomodeset//' "/boot/syslinux/syslinux.cfg"
+        echo
+        echo "#### Syslinux entry:"
+        grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
+    else
+        echo "You do not seem to use Syslinux. No configuration done."
+        echo "You have to remove 'nomodeset' kernel parameter manually."
+    fi
 else
     pacman -Rs --noconfirm ati-dri lib32-ati-dri  xf86-video-ati 
     pacman -Rdd --noconfirm libgl lib32-libgl 
     pacman -S  --noconfirm catalyst-utils catalyst-dkms lib32-catalyst-utils
     aticonfig --initial
-    sed -i '/^LABEL arch$/{n;n;n;s/$/ nomodeset/}' "/boot/syslinux/syslinux.cfg"
-    echo
-    echo "#### Syslinux entry:"
-    grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
+    if [ -f "/boot/syslinux/syslinux.cfg" ]; then
+        sed -i '/^LABEL arch$/{n;n;n;s/$/ nomodeset/}' "/boot/syslinux/syslinux.cfg"
+        echo
+        echo "#### Syslinux entry:"
+        grep -A4 "^LABEL arch$" "/boot/syslinux/syslinux.cfg"
+    else
+        echo "You do not seem to use Syslinux. No configuration done."
+        echo "You have to add 'nomodeset' kernel parameter manually."
+    fi
 fi
