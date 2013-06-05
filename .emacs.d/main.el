1651ea1f849336e2dc3d191e582dff3b2a8b895c
Pierre Neidhardt <ambrevar@gmail.com>
2013-06-05 16:19:50 +0200

    Emacs: improved GUD gdb-many-windows layout.

--- .emacs.d/main.el
+++ .emacs.d/main.el
@@ -207,3 +207,41 @@
 
 ;; Ediff in one frame.
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;; Change GUD many-windows layout.
+(defun gdb-setup-windows ()
+  "Layout the window pattern for `gdb-many-windows'."
+  (gdb-display-locals-buffer)
+  (gdb-display-stack-buffer)
+  (delete-other-windows)
+  (gdb-display-breakpoints-buffer)
+  (delete-other-windows)
+  (switch-to-buffer
+   (if gud-last-last-frame
+       (gud-find-file (car gud-last-last-frame))
+     (if gdb-main-file
+         (gud-find-file gdb-main-file)
+       ;; Put buffer list in window if we
+       ;; can't find a source file.
+       (list-buffers-noselect))))
+  (setq gdb-source-window (selected-window))
+  (split-window-horizontally)
+  (other-window 1)
+  (split-window nil ( / ( * (window-height) 3) 4))
+  (split-window nil ( / (window-height) 3))
+  (gdb-set-window-buffer (gdb-locals-buffer-name))
+  (other-window 1)
+  (pop-to-buffer gud-comint-buffer)
+  (when gdb-use-separate-io-buffer
+    (split-window-horizontally)
+    (other-window 1)
+    (gdb-set-window-buffer
+     (gdb-get-buffer-create 'gdb-inferior-io)))
+  (other-window 1)
+  (gdb-set-window-buffer (gdb-stack-buffer-name))
+  (split-window-horizontally)
+  (other-window 1)
+  (gdb-set-window-buffer (gdb-breakpoints-buffer-name))
+  (other-window 1))
+;; Set GUD to display many windows by default.
+(setq gdb-many-windows t)
