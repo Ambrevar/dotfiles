e0437ed0e1a87e0caf978ae35a06ca4be2577359
Pierre Neidhardt <ambrevar@gmail.com>
2013-07-03 21:39:45 +0200

    Emacs: multi-cursors bindings

--- .emacs.d/main.el
+++ .emacs.d/main.el
@@ -404,6 +404,8 @@ has errors and/or warnings."
       (setq mc/list-file (concat emacs-cache-folder "mc-lists.el"))
       ;; (define-key my-keys-minor-mode-map (kbd "C-c C-a") 'mc/edit-beginnings-of-lines)
       ;; (define-key my-keys-minor-mode-map (kbd "C-c C-e") 'mc/edit-ends-of-lines)
+      (global-unset-key (kbd "C-<down-mouse-1>"))
+      (define-key my-keys-minor-mode-map (kbd "C-<mouse-1>") 'mc/add-cursor-on-click)
       (define-key my-keys-minor-mode-map (kbd "C-c C-r") 'mc/edit-lines)
       (define-key my-keys-minor-mode-map (kbd "C-c C-n") 'mc/mark-next-like-this)
       ;; (define-key my-keys-minor-mode-map (kbd "C-c C-p") 'mc/mark-previous-like-this)
