8b111eaf1d3c395712d890b70bcc6c6081331b80
Pierre Neidhardt <ambrevar@gmail.com>
2013-07-09 19:08:31 +0200

    Emacs: removed unused cl function from mode-cc

--- .emacs.d/mode-cc.el
+++ .emacs.d/mode-cc.el
@@ -3,24 +3,6 @@
 ;;==============================================================================
 (require 'compile)
 
-;; Maybe we do not need common lisp for this. TODO: remove it when sure.
-(require 'cl) ; If you don't have it already
-(defun* get-closest-pathname-cl (&optional (file "Makefile"))
-  "Determine the pathname of the first instance of FILE starting
-from the current directory towards root.  This may not do the
-correct thing in presence of links. If it does not find FILE,
-then it shall return the name of FILE in the current directory,
-suitable for creation"
-  (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
-    (expand-file-name
-     file
-     (loop
-      for d = default-directory then (expand-file-name ".." d)
-      if (file-exists-p (expand-file-name file d))
-      return d
-      if (equal d root)
-      return nil))))
-
 (defun get-closest-pathname (&optional file)
   "Determine the pathname of the first instance of FILE starting
 from the current directory towards root.  This may not do the
