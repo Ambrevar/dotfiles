964d1c398534fb7d04a13f4fd15f43a20d68b2b4
Pierre Neidhardt <ambrevar@gmail.com>
2013-08-04 14:06:53 +0200

    ranger: extracthere command and unmap q

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -57,3 +57,38 @@ class bulkrename(Command):
         self.fm.execute_file([File(cmdfile.name)], app=local_ed)
         self.fm.run(['/bin/sh', cmdfile.name], flags='w')
         cmdfile.close()
+
+
+
+import os
+from ranger.core.loader import CommandLoader
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.env.copy)
+
+        if not copied_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.env.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = copied_files[0]
+        cwd = self.fm.env.cwd
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.env.copy.clear()
+        self.fm.env.cut = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
