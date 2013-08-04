01193372d24d5263108ede7032944de49aa97541
Pierre Neidhardt <ambrevar@gmail.com>
2013-08-04 16:07:33 +0200

    dwb: black about:blank, dns lookup

---
+++ .config/dwb/userscripts/aboutblack.js
@@ -0,0 +1,7 @@
+#!javascript
+
+signals.connect("documentLoaded", function (wv) {
+  if (wv.uri == "about:blank") {
+    wv.inject("document.body.style.background = '#000';");
+  }
+});
--- .config/dwb/userscripts/extension_loader.js
+++ .config/dwb/userscripts/extension_loader.js
@@ -27,3 +27,56 @@ filterListDir : "~/.config/dwb/adblock_lists"
 //>adblock_subscriptions___CONFIG
 });
 //>adblock_subscriptions___SCRIPT
+//<navtools___SCRIPT
+extensions.load("navtools", {
+//<navtools___CONFIG
+// Shortcut for navigating to the next page
+forwardBind : "]]",
+
+// Shortcut for navigating to the previous page
+backBind : "[[",
+
+// Go up one directory, e.g. from http://www.example.com/foo/bar to 
+// http://www.example.com/foo
+updirBind : "gu", 
+
+// Go to top directory, e.g. from http://www.example.com/foo/bar to 
+// http://www.example.com
+topdirBind : "gU", 
+
+// Patterns to match against when searching for "next"-links
+nextPatterns : "next,more,>,\u2192,\xbb,\u226b,>>",
+
+// Patterns to match against when searching for "previous"-links
+previousPatterns : "prev,previous,back,<,\u2190,\xab,\u226a,<<"
+
+//>navtools___CONFIG
+});
+//>navtools___SCRIPT
+//<autoquvi___SCRIPT
+/*<autoquvi___DISABLED
+extensions.load("autoquvi", {
+//<autoquvi___CONFIG
+  // The quvi command
+  quvi      : "quvi",  
+
+  // External player command
+  player    : "mplayer %u", 
+
+  // Whether to automatically play videos when quvi find a playable
+  // video
+  autoPlay  : true, 
+
+  // Whether to choose the quality before quvi starts
+  chooseQuality : true,
+
+  // A shortcut that spawns quvi for the current website
+  shortcut  : "",
+
+  // A command that spawns quvi for the current website 
+  command  : "autoquvi"
+
+//>autoquvi___CONFIG
+});
+autoquvi___DISABLED>*/
+//>autoquvi___SCRIPT
