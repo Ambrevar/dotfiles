c66141a6d9712f848121bc41922c5b3b06903ff1
Pierre Neidhardt <ambrevar@gmail.com>
2013-10-13 10:43:30 +0200

    mpv: improved integration

--- .config/dwb/userscripts/youtube-mplayer.js
+++ .config/dwb/userscripts/youtube-mplayer.js
@@ -6,6 +6,6 @@ var regex = new RegExp("http(.*)://www.youtube.com/watch\\?(.*&)*v=.*");
 
 signals.connect("navigation", function (wv, frame, request) {
     if (wv.mainFrame == frame && regex.test(request.uri)) 
-        system.spawn("sh -c 'mplayer \"$(youtube-dl -g " + request.uri + ")\"'");
+        system.spawn("sh -c 'mpv \"$(youtube-dl -g " + request.uri + ")\"'");
     return false;
 });
