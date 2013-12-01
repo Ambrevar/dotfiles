6901a44b7558f1f98272f5a574ff6b6a5f482e37
Pierre Neidhardt <ambrevar@gmail.com>
2013-12-02 00:46:44 +0100

    musish: init

---
+++ .config/musish/config.sh
@@ -0,0 +1,10 @@
+# ## Default script.
+# script="default"
+
+# ## Root of all output files.
+# library="$HOME/musics"
+
+## Terminal output. Turn it off when not redirecting to stdout.
+use_colors=true
+use_formatting=true
+
