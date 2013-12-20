83dd905cf0f78b09ca83385768e0eea95bddab18
Pierre Neidhardt <ambrevar@gmail.com>
2013-12-20 13:46:43 +0100

    musish: config update

--- .config/musish/config.sh
+++ .config/musish/config.sh
@@ -1,8 +1,9 @@
-# ## Default script.
-# script="default"
+## Default script.
+script="vorbis"
 
-# ## Root of all output files.
-# library="$HOME/musics"
+## Root of all output files.
+library="$HOME/musics"
+library_ext="/media/backup2/Musics"
 
 ## Terminal output. Turn it off when not redirecting to stdout.
 use_colors=true
