cc3e3c628c4049012b2a16de4bbaf482642417fa
Pierre Neidhardt <ambrevar@gmail.com>
2013-12-28 15:56:42 +0100

    dwb: fixed out-of-date scripts

--- .config/dwb/userscripts/aboutblack.js
+++ .config/dwb/userscripts/aboutblack.js
@@ -1,6 +1,6 @@
 #!javascript
 
-signals.connect("documentLoaded", function (wv) {
+Signal.connect("documentLoaded", function (wv) {
   if (wv.uri == "about:blank") {
     wv.inject("document.body.style.background = '#000';");
   }
--- .config/dwb/userscripts/youtube-mplayer.js
+++ .config/dwb/userscripts/youtube-mplayer.js
@@ -1,11 +1,11 @@
-//!javascript 
+//!javascript
 
 // Opens YouTube videos with mplayer.
 
 var regex = new RegExp("http(.*)://www.youtube.com/watch\\?(.*&)*v=.*");
 
-signals.connect("navigation", function (wv, frame, request) {
-    if (wv.mainFrame == frame && regex.test(request.uri)) 
+Signal.connect("navigation", function (wv, frame, request) {
+    if (wv.mainFrame == frame && regex.test(request.uri))
         system.spawn("sh -c 'mpv \"$(youtube-dl -g " + request.uri + ")\"'");
     return false;
 });
