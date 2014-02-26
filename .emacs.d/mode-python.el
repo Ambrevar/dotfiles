f0a43a9c19d43dc46267299f28e287177dd43bbd
Pierre Neidhardt <ambrevar@gmail.com>
2014-02-26 18:22:47 +0100

    Emacs: fixed python loading.

--- .emacs.d/mode-python.el
+++ .emacs.d/mode-python.el
@@ -2,14 +2,9 @@
 ;; Python
 ;;==============================================================================
 
-(defcustom python-interpreter nil
-  "Python interpreter. If value is nil, the shabang will be
-checked, and if no shabang is present, `python' will be used."
-  :safe 'stringp)
-
 (defun python-version ()
-  "Returns whether we're running Python 2 or 3 according to the
-shebang. System `python' is assumed by default."
+  "Returns \"python2\" or \"python3\" according to the shabang.
+`python-shell-interpreter' is assumed by default."
   (let ((firstline
          (car
           (split-string (buffer-substring-no-properties 1 (point-max)) "\n"))))
@@ -18,20 +13,15 @@ shebang. System `python' is assumed by default."
       (cond
        ((string-match "python2" firstline) "python2")
        ((string-match "python3" firstline) "python3")
-       (t "python")))))
+       (t python-shell-interpreter)))))
 
 (defun python-set-interpreter ()
   "Use compile to run python programs."
   (interactive)
-  (hack-local-variables)
-  (let ((py-interpreter
-         (if python-interpreter
-             python-interpreter
-           (python-version))))
-    (set (make-local-variable 'compile-command)
-         (concat py-interpreter " " buffer-file-name))))
+  (set (make-local-variable 'compile-command)
+       (concat (python-version) " " buffer-file-name)))
 
-(add-hook
+(add-hook-and-eval
  'python-mode-hook
  (lambda ()
    (set (make-local-variable 'compilation-scroll-output) t)
@@ -42,4 +32,4 @@ shebang. System `python' is assumed by default."
 ;; (add-to-list 'load-path "~/path/to/pydoc-info")
 ;; (require 'pydoc-info nil t)
 
-(provide 'python)
+(provide 'mode-python)
