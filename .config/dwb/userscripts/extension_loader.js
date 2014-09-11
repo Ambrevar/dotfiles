02c877b34a3168626489afeaf8b2156358597e54
Pierre Neidhardt <ambrevar@gmail.com>
2014-09-11 15:54:05 +0200

    dwb: Fix path to Adblock lists

--- .config/dwb/userscripts/extension_loader.js
+++ .config/dwb/userscripts/extension_loader.js
@@ -4,26 +4,26 @@ extensions.load("adblock_subscriptions", {
 //<adblock_subscriptions___CONFIG
 
 // Shortcut to subscribe to a filterlist
-scSubscribe : null,
+scSubscribe : null, 
 // Command to subscribe to a filterlist
-cmdSubscribe : "adblock_subscribe",
+cmdSubscribe : "adblock_subscribe", 
 
 // Shortcut to unsubscribe from a filterlist
-scUnsubscribe : null,
+scUnsubscribe : null, 
 
 // Command to unsubscribe from a filterlist
 cmdUnsubscribe : "adblock_unsubscribe",
 
 // Shortcut to update subscriptions and reload filter rules
 // Note that dwb will also update all subscriptions on startup
-scUpdate : null,
+scUpdate : null, 
 
 // Command to update subscriptions and reload filter rules
 // Note that dwb will also update all subscriptions on startup
-cmdUpdate : "adblock_update",
+cmdUpdate : "adblock_update", 
 
-// Path to the filterlist directory, will be created if it doesn't exist.
-filterListDir : "~/.config/dwb/adblock_lists"
+// Path to the filterlist directory, will be created if it doesn't exist. 
+filterListDir : data.configDir + "/adblock_lists"
 //>adblock_subscriptions___CONFIG
 });
 //>adblock_subscriptions___SCRIPT
