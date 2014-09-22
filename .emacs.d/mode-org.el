bfa95a5820f88d6d40a1a917e52f24a5e9ec1f76
Pierre Neidhardt <ambrevar@gmail.com>
2014-09-22 17:03:29 +0200

    Emacs: org settings

--- .emacs.d/mode-org.el
+++ .emacs.d/mode-org.el
@@ -1,8 +1,13 @@
 ;; Org mode config.
+
 ;; Move annoying babel folder. This move does not seem to work properly.
 (setq org-babel-temporary-directory (concat emacs-cache-folder "babel"))
 ;; Disable line splitting on M-RET
 (setq org-M-RET-may-split-line '((default)))
+
+(setq org-deadline-warning-days 7)
+(setq org-agenda-default-appointment-duration 60)
+(setq org-agenda-columns-add-appointments-to-effort-sum t)
 
 (setq org-agenda-files '("~/todo.org"))
 (setq org-enforce-todo-dependencies t)
@@ -17,6 +22,7 @@
 (add-hook
  'org-mode-hook
  (lambda ()
+   (local-set-key (kbd "C-c C-a") 'org-agenda)
    (auto-fill-mode -1)))
 
 (provide 'mode-org)
