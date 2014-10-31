3aed84329a04161cbbd8344dfd26f6185a4523d5
Pierre Neidhardt <ambrevar@gmail.com>
2014-10-31 15:11:37 +0100

    demlo: Update xray script

--- .config/demlo/scripts/xray.lua
+++ .config/demlo/scripts/xray.lua
@@ -1,53 +1,31 @@
 -- X-Ray Dog
--- WARNING: untested.
+-- WARNING: Untested.
 
 -- Remove all default tags.
-output.tags = {}
-o = output.tags
+tags = {}
 
-o.artist = 'X-Ray Dog'
+tags.artist = 'X-Ray Dog'
 -- Albums actually have years, but is it relevent since they have a code?
-o.date = ''
+tags.date = ''
 -- Genre could be Soundtrack, or Trailer Music. We choose '' for now.
-o.genre = ''
+tags.genre = ''
 
 -- Extract album code, assuming parent folder is 'XRCD## - $album'.
-XRCD = i.filename:match ('/[^/]*(XRCD%d*)[^/]*/[^/]*$')
+XRCD = output.filename:match('/[^/]*(XRCD%d*)[^/]*/[^/]*$')
 
 -- Append the album code to the constants array, other it will be capitalized.
-constants [XRCD:upper ()] = XRCD
+constants [XRCD:upper()] = XRCD
 
-o.album = i.album
-o.album:gsub ('XRCD - (%d+)', 'XRCD%1 - ')
+tags.album = not empty (o.album) and o.album:gsub ('XRCD - (%d+)', 'XRCD%1 - ') or 'Unknown album'
 
-o.title = i.title
-if empty (o.title) then
-   o.title = i.filename:match ('[^/]+$')
-   o.title = o.title:gsub ('%..+$', '')
-   o.title = o.title:gsub ('X-Ray Dog[%s-]*', '')
+tags.title = o.title
+if empty(tags.title) then
+	tags.title = output.filename:match('[^/]+$'):gsub('%..+$', ''):gsub('X-Ray Dog[%s-]*', '')
 end
 
-o.track = i.track
-if empty (o.track) then
-   o.track = o.title:match ('%d+')
+tags.track = o.track
+if empty(tags.track) then
+	tags.track = o.title:match('%d+')
 end
 
-cleantags (o)
-
--- Output filename
-output.filename = table.concat {library, '/', o.artist, '/',
-             not empty (o.album) and (empty (o.date) and o.album .. '/' or o.date .. ' - ' .. o.album .. '/') or '',
-             empty (track_padded) and '' or track_padded .. ' - ',
-             o.title}
-
--- Options
-if o.album then
-   output.cover_basename = o.album .. ' - Cover'
-else
-   output.cover_basename = 'Cover'
-end
-output.cover_clear_embedded = true
-output.cover_discard_high = false
-output.cover_discard_low = true
-output.cover_limit_low = 100
-output.tags_clear = true
+output.tags = tags
