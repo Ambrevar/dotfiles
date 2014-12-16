871801c26979902d096bc444fe5974c59ecad23d
Pierre Neidhardt <ambrevar@gmail.com>
2014-12-16 10:10:41 +0100

    Emacs: Asymptote mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,6 +78,12 @@ Example: to assign some-function to C-i, use
   (setq package-user-dir (concat emacs-cache-folder "elpa"))
   (package-initialize))
 
+(add-to-list 'load-path "/usr/share/asymptote")
+(autoload 'asy-mode "asy-mode.el" "Asymptote major mode." t)
+(autoload 'lasy-mode "asy-mode.el" "Hybrid Asymptote/Latex major mode." t)
+(autoload 'asy-insinuate-latex "asy-mode.el" "Asymptote insinuate LaTeX." t)
+(add-to-list 'auto-mode-alist '("\\.asy$" . asy-mode))
+
 (load-external "\\.bbcode\\'" 'bbcode-mode)
 (add-hook 'bbcode-mode-hook (lambda () (require 'mode-bbcode)))
 
