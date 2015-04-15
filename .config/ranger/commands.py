0c6df5b343c4fa8fa92caca1c82e4eb9d6d826bb
Pierre Neidhardt <ambrevar@gmail.com>
2015-04-15 08:00:03 +0200

    Ranger: Add toggle_flat command

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -124,3 +124,21 @@ class mkcd(Command):
                     self.fm.execute_console('scout -ae ^{}$'.format(s))
         else:
             self.fm.notify("file/directory exists!", bad=True)
+
+
+class toggle_flat(Command):
+    """
+    :toggle_flat
+
+    Flattens or unflattens the directory view.
+    """
+
+    def execute(self):
+        if self.fm.thisdir.flat == 0:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = -1
+            self.fm.thisdir.load_content()
+        else:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = 0
+            self.fm.thisdir.load_content()
