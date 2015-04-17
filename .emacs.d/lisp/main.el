cfac5bbea9a472b73a7f382910b5aaa4d2ede5ca
Pierre Neidhardt <ambrevar@gmail.com>
2015-04-17 16:45:56 +0200

    Emacs: Fix desktop-session loading condition

--- .emacs.d/lisp/main.el
+++ .emacs.d/lisp/main.el
@@ -280,7 +280,7 @@
 (setq inferior-lisp-program "clisp")
 
 ;; Let Emacs auto-load/save sessions.
-(when (boundp 'server-running-p)
+(when (and (fboundp 'server-running-p) (server-running-p))
   (desktop-save-mode 1)
   (setq history-length 250)
   (setq desktop-dirname (concat emacs-cache-folder "desktop"))
