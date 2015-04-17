c4855d3024e2a90db6c4b51faef3bc4002dfc2c7
Pierre Neidhardt <ambrevar@gmail.com>
2015-04-17 16:15:27 +0200

    Emacs: Emphasize matching parenthesis

--- .emacs.d/lisp/theme.el
+++ .emacs.d/lisp/theme.el
@@ -130,7 +130,8 @@
 
 ;; Show paren.
 (when (boundp 'show-paren-delay)
-  (set-face-background 'show-paren-match-face (face-background 'default))
+  ;; (set-face-background 'show-paren-match-face (face-background 'default)) ; Disable background color.
+  (set-face-background 'show-paren-match-face "#555555"))
   (set-face-foreground 'show-paren-match-face "#def")
   (set-face-attribute 'show-paren-match-face nil :weight 'extra-bold))
 
