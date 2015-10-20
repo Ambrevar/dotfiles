e7894edfd104d7e218411eaa9b343fa2f4476416
Pierre Neidhardt <ambrevar@gmail.com>
2015-10-20 12:05:50 +0200

    dwb: Delete config

--- .config/dwb/userscripts/aboutblack.js
+++
@@ -1,7 +0,0 @@
-#!javascript
-
-Signal.connect("documentLoaded", function (wv) {
-	if (wv.uri == "about:blank") {
-		wv.inject("document.body.style.background = '#000';");
-	}
-});
--- .config/dwb/userscripts/blinking-cursor.js
+++
@@ -1,7 +0,0 @@
-//!javascript
-
-Signal.connect("changeMode", function(wv, mode) {
-	if (mode == Modes.NormalMode) {
-		wv.focusedFrame.inject("document.activeElement.blur();");
-	}
-});
