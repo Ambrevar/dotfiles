7e8c34f12977b5e18017736d0e222fe1838ebe86
Pierre Neidhardt <ambrevar@gmail.com>
2015-12-09 11:02:23 +1100

    Emacs: Add go-eval-buffer and bindings

--- .emacs.d/lisp/mode-go.el
+++ .emacs.d/lisp/mode-go.el
@@ -2,13 +2,22 @@
 ;; Go
 ;;==============================================================================
 
+(defun go-eval-buffer ()
+  "Eval buffer with `go run'."
+  (interactive)
+  (let ((compile-command-backup compile-command))
+    (compile (concat "go run " (shell-quote-argument buffer-file-name)))
+    (setq compile-command compile-command-backup)))
+
 (add-hook-and-eval
  'go-mode-hook
  (lambda ()
    (add-hook 'before-save-hook #'gofmt-before-save nil t)
    (local-set-key (kbd "C-c m") 'go-main)
-   (local-set-key (kbd "C-c C-d") 'godoc)
-   (set (make-local-variable 'compile-command) (concat "go run " (shell-quote-argument buffer-file-name)))))
+   (local-set-key (kbd "C-c d") 'godoc)
+   (local-set-key (kbd "M-.") #'godef-jump)
+   (local-set-key (kbd "C-<f10>") 'go-eval-buffer)
+   (set (make-local-variable 'compile-command) "go build")))
 
 (define-skeleton go-main
   "Insert main function with basic includes."
