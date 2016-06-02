7eeebb3be4e119eb7481b3fc056159d3e7706c50
Pierre Neidhardt <ambrevar@gmail.com>
2016-06-02 22:28:00 +0200

    Emacs/Go: Use goimports, use 'go build' outside GOPATH, 'go install' in GOPATH

--- .emacs.d/lisp/mode-go.el
+++ .emacs.d/lisp/mode-go.el
@@ -1,8 +1,8 @@
 ;;==============================================================================
 ;; Go
 ;;==============================================================================
-
-(require 'go-autocomplete nil t)
+;; Packages: go-rename,
+;; Optional packages: go-autocomplete, go-guru
 
 (defun go-eval-buffer ()
   "Eval buffer with `go run'."
@@ -11,15 +11,25 @@
     (compile (concat "go run " (shell-quote-argument buffer-file-name)))
     (setq compile-command compile-command-backup)))
 
+(defun go-file-in-gopath-p ()
+  (let ((dir (expand-file-name (file-name-directory buffer-file-name))) (looping t) (gopath (getenv "GOPATH")))
+    (while (progn
+             (if (string= dir gopath)
+                 (setq looping nil)
+               (setq dir (expand-file-name ".." dir)))
+             (and looping (not (string= dir "/")))))
+    (if (string= dir "/") nil t)))
+
 (add-hook-and-eval
  'go-mode-hook
  (lambda ()
+   (setq gofmt-command "goimports")
    (add-hook 'before-save-hook #'gofmt-before-save nil t)
    (local-set-key (kbd "C-c m") 'go-main)
    (local-set-key (kbd "C-c d") 'godoc)
    (local-set-key (kbd "M-.") #'godef-jump)
    (local-set-key (kbd "C-<f10>") 'go-eval-buffer)
-   (set (make-local-variable 'compile-command) "go install")))
+   (set (make-local-variable 'compile-command) (if (go-file-in-gopath-p) "go install" "go build"))))
 
 (define-skeleton go-main
   "Insert main function with basic includes."
