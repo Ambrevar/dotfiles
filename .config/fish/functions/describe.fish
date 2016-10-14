8a717f202e3c65cb83be19f8dded52140619d72e
Pierre Neidhardt <ambrevar@gmail.com>
2016-10-14 09:26:03 +0530

    fish: Add pacman -Qo to 'describe'

--- .config/fish/functions/describe.fish
+++ .config/fish/functions/describe.fish
@@ -5,6 +5,7 @@ function describe -d 'Extend on "type" for executables'
 				set -l path (type -p $i)
 				ls -l $path
 				file $path | cut -d':' -f2 | cut -b 2-
+				type -pq pacman; and pacman -Qo $path
 			case '*'
 		type $i
 		end
