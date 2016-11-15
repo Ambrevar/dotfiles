9be2f570571ba94984074ef0d5c31a731e98030b
Pierre Neidhardt <ambrevar@gmail.com>
2016-11-15 16:51:20 +0530

    Demlo: Set bitrate dynamically in flac2ogg script

--- .config/demlo/scripts/51-flac2ogg.lua
+++ .config/demlo/scripts/51-flac2ogg.lua
@@ -1,7 +1,12 @@
 -- Keep result of 'encoding.lua' except flac.
 
-if output.format == 'flac' then
-	output.format = 'ogg'
-	output.parameters = {'-c:a', 'libvorbis', '-q:a', '10'}
-end
+local bitrate = bps or 9999999
+local OGGMAX = 500000
 
+if output.format == 'flac' then
+	if bitrate > input.bitrate then
+		bitrate = input.bitrate
+	end
+	output.format = 'ogg'
+	output.parameters = {'-c:a', 'libvorbis', '-b:a', tostring(math.min(bitrate, OGGMAX))}
+end
