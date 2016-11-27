b93a58f4c211b474f2961083ca1a332a4407f954
Pierre Neidhardt <ambrevar@gmail.com>
2016-11-27 16:49:25 +0530

    fzf: Add support for any file in 'describe'

--- .config/fish/functions/describe.fish
+++ .config/fish/functions/describe.fish
@@ -1,13 +1,21 @@
-function describe -d 'Extend on "type" for executables'
+function describe -d 'Extended "type"'
 	for i in $argv
-		switch (type -t $i)
+		set typ (type -t $i ^ /dev/null)
+		if test $status -ne 0
+			set -l path $i
+			ls -l $path
+			file $path | cut -d':' -f2 | cut -b 2-
+			type -pq pacman; and pacman -Qo $path ^ /dev/null
+			continue
+		end
+		switch $typ
 			case file
 				set -l path (type -p $i)
 				ls -l $path
 				file $path | cut -d':' -f2 | cut -b 2-
-				type -pq pacman; and pacman -Qo $path
+				type -pq pacman; and pacman -Qo $path ^ /dev/null
 			case '*'
-		type $i
+				type $i
 		end
 	end
 end
