1c62a0952387e8ad1405ec93e30585b628533401
Pierre Neidhardt <ambrevar@gmail.com>
2017-04-14 17:10:21 +0530

    fish: Remove unnecessary pacman aliases

--- .config/fish/pacman.fish
+++ .config/fish/pacman.fish
@@ -1,5 +1,3 @@
-## TODO: Remove unnecessary aliases.
-
 set -l listinstalled "(pacman -Q | string replace ' ' \t)"
 set -l listall "(__fish_print_packages)"
 
@@ -8,56 +6,14 @@ function pacfiles -d 'List of files in pacman package packages sorted by size'
 end
 complete -c pacfiles -a "$listinstalled"
 
-function pel
+function paced -d 'Edit files from packages'
 	set -l result
 	pacman -Qlq $argv | grep -v '/$' | eval (__fzfcmd) -m --tiebreak=index --toggle-sort=ctrl-r | while read -l r; set result $result $r; end
 	[ "$result" ]; and eval $EDITOR $result
 end
 complete -c pel -a "$listinstalled"
 
-function pi
-	sudo pacman -S --needed $argv
-end
-complete -c pi -a "$listall"
-
-function pli -a size -d 'Pacman last installed packages'
+function pacli -a size -d 'Pacman last installed packages'
 	[ $size ]; or set size 30
 	expac -t '%F %T' '%-8l %n' | sort -rn | head -$size
-end
-
-function pqi
-	pacman -Qi $argv
-end
-complete -c pqi -a "$listinstalled"
-
-function pql -d 'Get detailed file list (no folders) for the specified packages'
-	pacman -Ql $argv | grep -v '/$'
-end
-complete -c pql -a "$listinstalled"
-
-function pqo 
-	pacman -Qo $argv
-end
-complete -c pqo -a '(__fish_complete_command)'
-
-function pqs
-	pacman -Qs $argv
-end
-
-function pr
-	sudo pacman -Rs $argv
-end
-complete -c pr -a "$listinstalled"
-
-function psi
-	pacman -Si $argv
-end
-complete -c psi -a "$listall"
-
-function pss
-	pacman -Ss $argv
-end
-
-function pu -d 'Run pacman system update'
-	yes | sudo pacman -Sc; and sudo pacman -Syu
 end
