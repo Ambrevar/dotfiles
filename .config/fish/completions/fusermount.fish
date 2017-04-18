905ffb79c31cf776310b6522918eea1324196fc4
Pierre Neidhardt <ambrevar@gmail.com>
2017-04-18 16:05:10 +0530

    fish: Delete merged 'fusermount' completion fix

--- .config/fish/completions/fusermount.fish
+++
@@ -1,9 +0,0 @@
-## TODO: Remove this fix when upstream has merged it.
-## https://github.com/fish-shell/fish-shell/pull/3642
-complete -c fusermount --description "Mount point" -x -a '(__fish_print_mounted)'
-complete -c fusermount -s h --description "Display help and exit"
-complete -c fusermount -s v --description "Display version and exit"
-complete -c fusermount -s o -x --description "Mount options"
-complete -c fusermount -s u --description "Unmount"
-complete -c fusermount -s q --description "Quiet"
-complete -c fusermount -s z --description "Lazy unmount"
