1193a76114572c036112531aa34808241e83d580
Pierre Neidhardt <ambrevar@gmail.com>
2017-05-09 22:42:38 +0530

    fish: Add \ee edit_command_buffer binding in insert mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,14 +2,18 @@ function fish_user_key_bindings
 	fish_vi_key_bindings
 	set fish_escape_delay_ms 10
 
+	## Useful for rsync when folders are completed with fzf.
+	bind \cw trim_trailing_slashes
+
+	## As of fish 2.5, Alt-f binding is missing in vi-mode.
+	bind -M insert \ef forward-word
+
+	bind -M insert \ee edit_command_buffer
+
 	## fzf
 	if type -pq fzf
 		source $fish_config_path/fzf.fish
 	end
 
-	# bind \el downcase-word # For Emacs bindings only.
-	bind \cw trim_trailing_slashes
-
-	## As of fish 2.5, Alt-f binding is missing in vi-mode.
-	bind -M insert \ef forward-word
+	# bind \el downcase-word # For Emacs bindings only, load after fzf.
 end
