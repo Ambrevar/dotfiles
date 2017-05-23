dfeb676474f24b6fc8b5ac7d8636425eb49ed088
Pierre Neidhardt <ambrevar@gmail.com>
2017-05-23 17:15:50 +0200

    demlo: Set ogg max bitrate to 192k

--- .config/demlo/scripts/51-flac2ogg.lua
+++ .config/demlo/scripts/51-flac2ogg.lua
@@ -1,12 +1,12 @@
 -- Keep result of 'encoding.lua' except flac.
 
 local bitrate = bps or 9999999
-local OGGMAX = 500000
+local bitrate_max = 192000
 
 if output.format == 'flac' then
 	if bitrate > input.bitrate then
 		bitrate = input.bitrate
 	end
 	output.format = 'ogg'
-	output.parameters = {'-c:a', 'libvorbis', '-b:a', tostring(math.min(bitrate, OGGMAX))}
+	output.parameters = {'-c:a', 'libvorbis', '-b:a', tostring(math.min(bitrate, bitrate_max))}
 end
--- .config/demlo/scripts/52-ogg.lua
+++ .config/demlo/scripts/52-ogg.lua
@@ -1,14 +1,12 @@
 -- Force output to OGG.
 
-local OGGMAX = 500000
-
 local bitrate = bps or 9999999
-local OGGMAX = 500000
+local bitrate_max = 192000
 
 if bitrate > input.bitrate then
 	bitrate = input.bitrate
 end
 
-output.parameters = {'-c:a', 'libvorbis', '-b:a', tostring(math.min(bitrate, OGGMAX))}
+output.parameters = {'-c:a', 'libvorbis', '-b:a', tostring(math.min(bitrate, bitrate_max))}
 
 output.format = 'ogg'
