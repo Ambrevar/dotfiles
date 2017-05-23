b8794f4f1a2af397be3a2285075c9c88f6e162c2
Pierre Neidhardt <ambrevar@gmail.com>
2017-05-23 17:48:46 +0200

    fish: Fix escaping of ~ in trim_trailing_slashes

--- .config/fish/functions/trim_trailing_slashes.fish
+++ .config/fish/functions/trim_trailing_slashes.fish
@@ -4,7 +4,14 @@ function trim_trailing_slashes --description "Trim trailing slashes of all comma
 	set -l list (commandline -o)
 	commandline -r ""
 	for i in $list
-		commandline -i -- (string escape (string replace -r '/$' '' $i))
+		set i (string replace -r '/$' '' $i)
+		set -l prefix (string sub -s 1 -l 1 -- $i)
+		switch $prefix
+			case '~'
+				commandline -t -- (string sub -s 2 (string escape -n -- $i))
+			case '*'
+				commandline -t -- (string escape -- $i)
+		end
 		commandline -i -- ' '
  	end
 end
