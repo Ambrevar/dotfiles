ae6fe9dd77a5a10608fe2c8018e79625d0ade61f
Pierre Neidhardt <ambrevar@gmail.com>
2017-06-03 11:44:50 +0100

    Emacs: Insert org-mode heading after current entry instead of splitting

--- .emacs.d/lisp/mode-org.el
+++ .emacs.d/lisp/mode-org.el
@@ -1,24 +1,25 @@
-;; Org mode config.
+;;; Org mode
 
 (local-set-key (kbd "C-c C-a") 'org-agenda)
 
-;; Move annoying babel folder. This move does not seem to work properly.
+;;; Move annoying babel folder.
 (setq org-babel-temporary-directory (concat emacs-cache-folder "babel"))
-;; Disable line splitting on M-RET
+;;; Disable line splitting on M-RET
 (setq org-M-RET-may-split-line '((default)))
+(setq org-insert-heading-respect-content t)
+(setq org-enforce-todo-dependencies t)
 
 (setq org-deadline-warning-days 7)
 (setq org-agenda-default-appointment-duration 60)
 (setq org-agenda-columns-add-appointments-to-effort-sum t)
 
-;; Agendas.
-;; If you want to add other agendas in a local file, use the following code:
-; (with-eval-after-load "org"
-;   (add-to-list 'org-agenda-files "/path/to/agenda.org"))
+;;; Agendas.
+;;; If you want to add other agendas in a local file, use the following code:
+;; (with-eval-after-load "org"
+;;   (add-to-list 'org-agenda-files "/path/to/agenda.org"))
 (add-to-list 'org-agenda-files "~/personal/todo/todo.org")
 
-(setq org-enforce-todo-dependencies t)
-;; Set PDF association in Org-mode (was Evince by default).
+;;; Set PDF association in Org-mode (was Evince by default).
 (with-eval-after-load "org"
   (setq indent-tab-mode nil) ; Org-mode aligns text.
   (require 'tool-pdf)
