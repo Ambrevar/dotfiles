04316d379dde01bd4f6849853097f674c2ebcc12
Pierre Neidhardt <ambrevar@gmail.com>
2017-06-06 18:46:27 +0100

    fish/fzf: Complete subcommands (sudo, env)

--- .config/fish/fzf.fish
+++ .config/fish/fzf.fish
@@ -33,7 +33,25 @@ bind m fzf-select
 ## - ~/
 ## - echo \$HOME-$H<tab>
 function fzf-complete -d 'fzf completion and print selection back to commandline'
-	set -l complist (complete -C(commandline -c))
+	# As of 2.6, fish's "complete" function does not understand
+	# subcommands. Instead, we use the same hack as __fish_complete_subcommand and
+	# extract the subcommand manually.
+	set -l cmd (commandline -co) (commandline -ct)
+	switch $cmd[1]
+		case env sudo
+			for i in (seq 2 (count $cmd))
+				switch $cmd[$i]
+					case '-*'
+					case '*=*'
+					case '*'
+						set cmd $cmd[$i..-1]
+						break
+				end
+			end
+	end
+	set cmd (string join -- ' ' $cmd)
+
+	set -l complist (complete -C$cmd)
 	set -l result
 	string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
 
