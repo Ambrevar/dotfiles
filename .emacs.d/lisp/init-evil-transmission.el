fec3bdd6fe56cb7de4967214e67af16de41bc562
Pierre Neidhardt <ambrevar@gmail.com>
2017-08-31 19:47:24 +0100

    Evil: Fix transmission initial states

--- .emacs.d/lisp/init-evil-transmission.el
+++ .emacs.d/lisp/init-evil-transmission.el
@@ -1,7 +1,6 @@
 ;;; Evil+Transmission
 
 (evil-set-initial-state 'transmission-mode 'motion)
-
 (evil-define-key 'motion transmission-mode-map
   (kbd "<return>") 'transmission-files
   "D" 'transmission-delete
@@ -20,6 +19,7 @@
   "c" 'transmission-verify
   "C" 'transmission-set-bandwidth-priority)
 
+(evil-set-initial-state 'transmission-files-mode 'motion)
 (evil-define-key 'motion transmission-files-mode-map
   (kbd "<return>") 'transmission-find-file
   "\M-l" 'transmission-display-file
@@ -38,6 +38,7 @@
   "U" 'transmission-files-want
   "C" 'transmission-files-priority)
 
+(evil-set-initial-state 'transmission-info-mode 'motion)
 (evil-define-key 'motion transmission-info-mode-map
   "r" 'transmission-trackers-remove
   "c" 'transmission-copy-magnet
@@ -51,6 +52,7 @@
   "I" 'transmission-trackers-add
   "C" 'transmission-set-bandwidth-priority)
 
+(evil-set-initial-state 'transmission-peers-mode 'motion)
 (evil-define-key 'motion transmission-peers-mode-map
   "S" 'tabulated-list-sort
   "i" 'transmission-info
