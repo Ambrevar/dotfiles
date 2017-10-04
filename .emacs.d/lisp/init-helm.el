0283a57d183edac999f3985fde10bab7a8e303db
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-04 12:28:55 +0100

    Helm: Use "mosaic" windowing

--- .emacs.d/lisp/init-helm.el
+++ .emacs.d/lisp/init-helm.el
@@ -183,4 +183,6 @@ Requires `call-process-to-string' from `functions'."
 ;;; Column indices might need some customizing. See `helm-top-command' and
 ;;; https://github.com/emacs-helm/helm/issues/1586.
 
+(setq helm-window-show-buffers-function 'helm-window-mosaic-fn)
+
 (provide 'init-helm)
