63f7efd817ffc09b08a15ecae78d19e2833b9b62
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-17 18:21:45 +0100

    Evil/Org: Overhaul org-agenda

--- .emacs.d/lisp/init-evil-org.el
+++ .emacs.d/lisp/init-evil-org.el
@@ -23,91 +23,118 @@
   (kbd "M-L") 'org-shiftmetaright
   "<" 'org-up-element)
 
+;; Horizontal movements have little use, so we override "f" and "t", ";" and
+;; ",", "|".  We keep "w", "b", "e", "ge" and the upcase versions.
 (evil-set-initial-state 'org-agenda-mode 'motion)
 (evil-define-key 'motion org-agenda-mode-map
+  ;; TODO: Bind date selection from miniprompt to S-<hjkl>.
+  ;; TODO: Unused keys: D, x, X, o, p, P
+
   (kbd "<tab>") 'org-agenda-goto
   (kbd "<return>") 'org-agenda-switch-to
+  (kbd "S-<return>") 'org-agenda-recenter
+
   (kbd "<space>") 'org-agenda-show-and-scroll-up
   (kbd "<delete>") 'org-agenda-show-scroll-down
   (kbd "<backspace>") 'org-agenda-show-scroll-down
-  "dd" 'org-agenda-kill
-  "u" 'org-agenda-undo
-  "!" 'org-agenda-toggle-deadlines
-  "zd" 'org-agenda-dim-blocked-tasks
-  "za" 'org-agenda-archive
-  "zr" 'org-agenda-bulk-mark-regexp
-  "zm" 'org-agenda-bulk-mark-all
-  "+" 'org-agenda-priority-up
-  "-" 'org-agenda-priority-down
-  ;; "," 'org-agenda-priority
-  "." 'org-agenda-goto-today
-  (kbd "M-s f") 'org-agenda-filter-by-tag
-  "s" 'org-agenda-set-tags
-  "S" 'org-agenda-set-timer
-  "<" 'org-agenda-filter-by-category
-  "=" 'org-agenda-filter-by-regexp
-  ">" 'org-agenda-date-prompt
-  "zf" 'org-agenda-show-the-flagging-note
-  "A" 'org-agenda-append-agenda
-  "zb" 'org-agenda-bulk-action
-  "C" 'org-agenda-convert-date
-  "D" 'org-agenda-toggle-diary
-  "ge" 'org-agenda-entry-text-mode
-  "zf" 'org-agenda-follow-mode
-  "zg" 'org-agenda-toggle-time-grid
-  "gh" 'org-agenda-holidays
-  "I" 'org-agenda-clock-in
-  "O" 'org-agenda-clock-out
-  "J" 'org-agenda-clock-goto
-  ;; "L" 'org-agenda-recenter
-  "gm" 'org-agenda-phases-of-moon
-  (kbd "C-j") 'org-agenda-next-item
-  (kbd "C-k") 'org-agenda-previous-item
-  ;; "Q" 'org-agenda-quit
-  "q" 'org-agenda-quit
-  "R" 'org-agenda-clockreport-mode
-  "gs" 'org-agenda-sunrise-sunset
-  "gi" 'org-agenda-show-tags
-  "zM" 'org-agenda-bulk-unmark-all
-  "X" 'org-agenda-clock-cancel
-  "[" 'org-agenda-manipulate-query-add
-  "]" 'org-agenda-manipulate-query-subtract
-  "z^" 'org-agenda-filter-by-top-headline
-  "z_" 'org-agenda-filter-by-effort
-  "a" 'org-agenda-archive-default-with-confirmation
-  (kbd "M-h") 'org-agenda-earlier
-  (kbd "M-l") 'org-agenda-later
-  "c" 'org-agenda-goto-calendar
-  ;; "d" 'org-agenda-day-view
-  ;; "se" 'org-agenda-set-effort
-  "gr" 'org-agenda-redo-all
-  (kbd "C-r") 'org-agenda-redo
-  "i" 'org-agenda-diary-entry
-  "gd" 'org-agenda-goto-date
-  "zk" 'org-agenda-capture
-  "zl" 'org-agenda-log-mode
-  ;; "zm" 'org-agenda-bulk-mark
+
+  ;; Motion
   "j" 'org-agenda-next-line
   "k" 'org-agenda-previous-line
-  ;; "s" 'org-save-all-org-buffers
-  "gt" 'org-agenda-todo
-  ;; "zM" 'org-agenda-bulk-unmark
-  "zv" 'org-agenda-view-mode-dispatch
-  "gw" 'org-agenda-week-view
-  "Q" 'org-agenda-exit
-  "gy" 'org-agenda-year-view
-  ;; "za" 'org-agenda-add-note
-  "{" 'org-agenda-manipulate-query-add
-  "|" 'org-agenda-filter-remove-all ; goto-column?
-  "}" 'org-agenda-manipulate-query-subtract
-  "~" 'org-agenda-limit-interactively ; invert-char?
-  "H" 'org-agenda-do-date-earlier
+  (kbd "]") 'org-agenda-earlier
+  (kbd "[") 'org-agenda-later
+  (kbd "C-k") 'org-agenda-previous-item
+  (kbd "C-j") 'org-agenda-next-item
+
+  ;; Manipulation
+  ;; We follow standard org-mode bindings (not org-agenda bindings):
+  ;; <HJKL> change todo items and priorities.
+  ;; M-<jk> drag lines.
+  ;; M-<hl> cannot demote/promote, we use it for "do-date".
+  "H" 'org-agenda-todo-previousset
+  "L" 'org-agenda-todo-nextset
   "J" 'org-agenda-priority-down
   "K" 'org-agenda-priority-up
-  "L" 'org-agenda-do-date-later
-  (kbd "M-H") 'org-agenda-todo-previousset
-  (kbd "M-J") 'org-agenda-drag-line-forward
-  (kbd "M-K") 'org-agenda-drag-line-backward
-  (kbd "M-L") 'org-agenda-todo-nextset)
+  (kbd "M-j") 'org-agenda-drag-line-forward
+  (kbd "M-k") 'org-agenda-drag-line-backward
+  (kbd "M-h") 'org-agenda-do-date-earlier
+  (kbd "M-l") 'org-agenda-do-date-later
+
+  ;; Undo
+  "u" 'org-agenda-undo
+  "U" 'org-agenda-redo-all
+  (kbd "C-r") 'org-agenda-redo
+
+  ;; Actions
+  "dd" 'org-agenda-kill
+  "dA" 'org-agenda-archive
+  "da" 'org-agenda-archive-default-with-confirmation
+  "st" 'org-agenda-set-tags
+  "se" 'org-agenda-set-effort
+  "S" 'org-timer-set-timer
+  "i" 'org-agenda-diary-entry
+  "a" 'org-agenda-add-note
+  "A" 'org-agenda-append-agenda
+  "C" 'org-agenda-capture
+
+  ;; Marking
+  "mm" 'org-agenda-bulk-toggle
+  "mM" 'org-agenda-bulk-toggle-all
+  "m%" 'org-agenda-bulk-mark-regexp
+  "mu" 'org-agenda-bulk-remove-all-marks
+  "ma" 'org-agenda-bulk-action
+
+  ;; Quit
+  "ZQ" 'org-agenda-exit
+  "ZZ" 'org-agenda-quit
+
+  ;; Display
+  ;; "Dispatch" can prefix the following:
+  ;; 'org-agenda-toggle-deadlines
+  ;; 'org-agenda-toggle-diary
+  ;; 'org-agenda-follow-mode
+  ;; 'org-agenda-log-mode
+  ;; 'org-agenda-entry-text-mode
+  ;; 'org-agenda-toggle-time-grid
+  ;; 'org-agenda-day-view
+  ;; 'org-agenda-week-view
+  ;; 'org-agenda-year-view
+  "z" 'org-agenda-view-mode-dispatch
+  "ZD" 'org-agenda-dim-blocked-tasks
+
+  ;; Filter
+  "fc" 'org-agenda-filter-by-category
+  "fr" 'org-agenda-filter-by-regexp
+  "fe" 'org-agenda-filter-by-effort
+  "ft" 'org-agenda-filter-by-tag
+  "fu" 'org-agenda-filter-remove-all
+  "f^" 'org-agenda-filter-by-top-headline
+  "~" 'org-agenda-limit-interactively
+
+  ;; Clock
+  "I" 'org-agenda-clock-in
+  "O" 'org-agenda-clock-out
+  "cg" 'org-agenda-clock-goto
+  "cc" 'org-agenda-clock-cancel
+  "cr" 'org-agenda-clockreport-mode
+
+  ;; Go and show
+  "." 'org-agenda-goto-today ; TODO: What about evil-repeat?
+  "gc" 'org-agenda-goto-calendar
+  "gC" 'org-agenda-convert-date
+  "gd" 'org-agenda-goto-date
+  "gh" 'org-agenda-holidays
+  "gm" 'org-agenda-phases-of-moon
+  "gs" 'org-agenda-sunrise-sunset
+  "gt" 'org-agenda-show-tags
+
+  ;; Others
+  "+" 'org-agenda-manipulate-query-add
+  "-" 'org-agenda-manipulate-query-subtract
+  ;; TODO: Work out the following.
+  ;; 'org-agenda-date-prompt
+  ;; 'org-agenda-show-the-flagging-note
+  ;; 'org-save-all-org-buffers
+  )
 
 (provide 'init-evil-org)
