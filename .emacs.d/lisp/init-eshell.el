69e484a87faa30157c2a63f7e5d7b067fc07ec23
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-25 23:36:12 +0100

    Eshell: Prepare fish completion for packaging

--- .emacs.d/lisp/init-eshell.el
+++ .emacs.d/lisp/init-eshell.el
@@ -204,21 +204,51 @@ See `eshell' for the numeric prefix ARG."
 ;;; "complete", but that's brittle because of unreliable shell escaping.
 ;;; Upstream does not allow for skipping the user config:
 ;;; https://github.com/fish-shell/fish-shell/issues/4165.
-(when (executable-find "fish")
-  (setq eshell-default-completion-function 'eshell-fish-completion))
+(defvar fish-completion-command "fish"
+  "The `fish' executable.")
 
-(defun eshell-fish-completion ()
+;; TODO: Make minor mode for buffer-local completion?  Probably not worth it.
+(defun fish-completion-eshell-global-toggle ()
+  "Turn on/off fish shell completion in all future Eshells.
+Eshell fallbacks on fish whenever it cannot complete normally."
+  (interactive)
+  (if (or (eq eshell-default-completion-function 'fish-completion-eshell-complete)
+          (not (executable-find fish-completion-command)))
+      (progn
+        (setq eshell-default-completion-function (eval (car (get 'eshell-default-completion-function 'standard-value))))
+        (message "fish completion disabled in all future Eshells"))
+    (setq eshell-default-completion-function 'fish-completion-eshell-complete)
+    (message "fish completion enabled in all future Eshells")))
+
+(defun fish-completion-eshell-toggle ()
+  "Turn on/off fish shell completion in current Eshell.
+Eshell fallbacks on fish whenever it cannot complete normally."
+  (interactive)
+  (if (or (eq pcomplete-default-completion-function 'fish-completion-eshell-complete)
+          (not (executable-find fish-completion-command)))
+      (progn
+        (setq pcomplete-default-completion-function (eval (car (get 'eshell-default-completion-function 'standard-value))))
+        (message "fish completion disabled in current Eshell"))
+    (setq pcomplete-default-completion-function 'fish-completion-eshell-complete)
+    (message "fish completion enabled in current Eshell")))
+
+(defun fish-completion-eshell-complete ()
+  "Complete Eshell's prompt with `fish-completion-complete'."
+  (fish-completion-complete (buffer-substring-no-properties
+                             (save-excursion (eshell-bol) (point)) (point))))
+
+(defun fish-completion-complete (raw-prompt)
+  "Complete RAW-PROMPT (any string) using the fish shell."
   (while (pcomplete-here
           (let ((comp-list
-                 (let* ((raw-prompt (buffer-substring-no-properties (save-excursion (eshell-bol) (point)) (point)))
-                        ;; Keep spaces at the end with OMIT-NULLS=nil in `split-string'.
+                 (let* (;; Keep spaces at the end with OMIT-NULLS=nil in `split-string'.
                         (toks (split-string raw-prompt split-string-default-separators nil))
-                        ;; The first non-empty `car' is the command. Discard
+                        ;; The first non-empty `car' is the command.  Discard
                         ;; leading empty strings.
                         (tokens (progn (while (string= (car toks) "")
                                          (setq toks (cdr toks)))
                                        toks))
-                        ;; Fish does not support subcommand completion. We make
+                        ;; Fish does not support subcommand completion.  We make
                         ;; a special case of 'sudo' and 'env' since they are
                         ;; the most common cases involving subcommands.  See
                         ;; https://github.com/fish-shell/fish-shell/issues/4093.
@@ -232,14 +262,14 @@ See `eshell' for the numeric prefix ARG."
                                     (setq tokens (cdr tokens)))
                                   (mapconcat 'identity tokens " "))))
                    ;; Completion result can be a filename.  pcomplete expects
-                   ;; cannonical file names (i.e. withou '~') while fish preserves
+                   ;; cannonical file names (i.e. without '~') while fish preserves
                    ;; non-cannonical results.  If the result contains a directory,
                    ;; expand it.
                    (mapcar (lambda (e) (car (split-string e "\t")))
                            (split-string
                             (with-output-to-string
                               (with-current-buffer standard-output
-                                (call-process "fish" nil t nil "-c" (format "complete -C'%s'" prompt))))
+                                (call-process fish-completion-command nil t nil "-c" (format "complete -C'%s'" prompt))))
                             "\n" t)))))
             (if (and comp-list (file-name-directory (car comp-list)))
                 (pcomplete-dirs-or-entries)
