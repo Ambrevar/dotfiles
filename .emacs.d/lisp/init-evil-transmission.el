d3045b336557ef8d278f2f48901190a304d25714
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-27 15:04:19 +0100

    Evil: Clean up transmission for consistency

--- .emacs.d/lisp/init-evil-transmission.el
+++ .emacs.d/lisp/init-evil-transmission.el
@@ -2,60 +2,104 @@
 
 (evil-set-initial-state 'transmission-mode 'motion)
 (evil-define-key 'motion transmission-mode-map
-  (kbd "<return>") 'transmission-files
-  "D" 'transmission-delete
-  "S" 'tabulated-list-sort
+  (kbd "SPC") 'scroll-up-command
+  (kbd "S-SPC") 'scroll-down-command
+  (kbd "<delete>") 'scroll-down-command
+
+  ;; sort
+  "s" 'tabulated-list-sort
+
+  ;; "D" 'transmission-delete ; TODO: useless with `transmission-remove'?
   "a" 'transmission-add
-  "d" 'transmission-set-download
+  "p" 'transmission-move
+  "d" 'transmission-remove
+
+  (kbd "<return>") 'transmission-files
   "e" 'transmission-peers
   "i" 'transmission-info
-  "U" 'transmission-set-ratio
-  "x" 'transmission-move
-  "q" 'transmission-quit
-  "r" 'transmission-remove
-  "s" 'transmission-toggle
-  "I" 'transmission-trackers-add
-  "u" 'transmission-set-upload
+
+  "x" 'transmission-toggle ; TODO: Match EMMS' pause.
+  "A" 'transmission-trackers-add
   "c" 'transmission-verify
-  "C" 'transmission-set-bandwidth-priority)
+  "D" 'transmission-set-download
+  "U" 'transmission-set-upload
+  "R" 'transmission-set-ratio
+  "P" 'transmission-set-bandwidth-priority
+
+  ;; mark
+  "m" 'transmission-toggle-mark
+  "M" 'transmission-unmark-all
+  "~" 'transmission-invert-marks
+
+  ;; update
+  "gr" 'revert-buffer
+
+  ;; quit
+  "q" 'transmission-quit
+  "ZQ" 'transmission-quit
+  "ZZ" 'transmission-quit)
 
 (evil-set-initial-state 'transmission-files-mode 'motion)
 (evil-define-key 'motion transmission-files-mode-map
-  (kbd "<return>") 'transmission-find-file
-  (kbd "M-l") 'transmission-display-file
-  "!" 'transmission-files-command
-  "S" 'tabulated-list-sort
-  "A" 'transmission-browse-url-of-file
-  "X" 'transmission-files-command
-  "^" 'quit-window
+  (kbd "SPC") 'scroll-up-command
+  (kbd "S-SPC") 'scroll-down-command
+  (kbd "<delete>") 'scroll-down-command
+
+  ;; sort
+  "s" 'tabulated-list-sort
+
   "e" 'transmission-peers
   "i" 'transmission-info
-  "x" 'transmission-move
+
+  (kbd "<return>") 'transmission-find-file
+  (kbd "S-<return>") 'transmission-display-file
+  "!" 'transmission-files-command
+  ;; "X" 'transmission-files-command
+  "A" 'transmission-browse-url-of-file
   "o" 'transmission-find-file-other-window
-  "q" 'quit-window
-  "u" 'transmission-files-unwant
   "O" 'transmission-view-file
+
+  "p" 'transmission-move
+  "u" 'transmission-files-unwant
   "U" 'transmission-files-want
-  "C" 'transmission-files-priority)
+  "P" 'transmission-files-priority
+
+  ;; quit
+  "q" 'quit-window
+  "ZQ" 'transmission-quit
+  "ZZ" 'transmission-quit)
 
 (evil-set-initial-state 'transmission-info-mode 'motion)
 (evil-define-key 'motion transmission-info-mode-map
-  "r" 'transmission-trackers-remove
-  "c" 'transmission-copy-magnet
-  "d" 'transmission-set-torrent-download
-  "U" 'transmission-set-torrent-ratio
-  "q" 'quit-window
-  "a" 'transmission-trackers-add
-  "u" 'transmission-set-torrent-upload
   "e" 'transmission-peers
-  "x" 'transmission-move
-  "I" 'transmission-trackers-add
-  "C" 'transmission-set-bandwidth-priority)
+
+  "A" 'transmission-trackers-add
+  "r" 'transmission-trackers-remove ; TODO: Add to the main view as well? Bad binding.
+
+  "D" 'transmission-set-torrent-download
+  "U" 'transmission-set-torrent-upload
+  "R" 'transmission-set-torrent-ratio
+  "P" 'transmission-set-bandwidth-priority
+
+  "c" 'transmission-copy-magnet ; TODO: Should be `transmission-verify' like in the main view?
+
+  "p" 'transmission-move
+
+  ;; quit
+  "q" 'quit-window
+  "ZQ" 'quit-window
+  "ZZ" 'quit-window)
 
 (evil-set-initial-state 'transmission-peers-mode 'motion)
 (evil-define-key 'motion transmission-peers-mode-map
-  "S" 'tabulated-list-sort
+  ;; sort
+  "s" 'tabulated-list-sort
+
   "i" 'transmission-info
-  "q" 'quit-window)
+
+  ;; quit
+  "q" 'quit-window
+  "ZQ" 'quit-window
+  "ZZ" 'quit-window)
 
 (provide 'init-evil-transmission)
