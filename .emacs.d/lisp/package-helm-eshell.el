2eed37474921b5f5f17021eb8e8ed833d1559e27
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-27 18:19:36 +0100

    Helm/Eshell: Add helm-eshell-browse-all-prompts

--- .emacs.d/lisp/package-helm-eshell.el
+++ .emacs.d/lisp/package-helm-eshell.el
@@ -1,5 +1,16 @@
 ;;; TODO: Commit upstream.
-;;; TODO: Global version?
+;;; TODO: Support multiline prompts?
+;;; TODO: Add colored numbers and color shell names.
+
+(defface helm-eshell-promptno
+  '((t (:foreground "Darkorange1")))
+  "Face used to highlight Eshell prompt index."
+  :group 'helm-eshell-faces)
+
+(defface helm-eshell-buffer-name
+  '((t (:foreground "Green")))
+  "Face used to highlight Eshell buffer name."
+  :group 'helm-eshell-faces)
 
 (defun helm-eshell-prompt-list ()
   "Return an alist of (prompt . pos)."
@@ -11,12 +22,37 @@
         (setq list (cons `(,(buffer-substring-no-properties (point) (line-end-position)) . ,(point)) list))))
     list))
 
+(defun helm-eshell-all-prompt-list ()
+  "Return an alist of (\"buffer-name:prompt\" . (buffer . pos))."
+  (let (list)
+    (dolist (b (buffer-list))
+      (with-current-buffer b
+        (when (eq major-mode 'eshell-mode)
+          (save-excursion
+            (beginning-of-buffer)
+            (let ((buffer-name (buffer-name))
+                  (count 1))
+              (while (not (eobp))
+                (eshell-next-prompt 1)
+                (setq list (cons `(,(format "%s:%s:%s"
+                                            buffer-name
+                                            count
+                                            (buffer-substring-no-properties (point) (line-end-position)))
+                                   . (,b . ,(point)))
+                                 list))
+                (setq count (1+ count))))))))
+    list))
+
 (defun helm-eshell-goto-prompt (candidate)
   (goto-char candidate))
 
+(defun helm-eshell-goto-all-prompt (candidate)
+  (switch-to-buffer (car candidate))
+  (goto-char (cdr candidate)))
+
 (with-eval-after-load 'helm
   (defun helm-eshell-browse-prompts ()
-    "Preconfigured `helm' to browse all prompts."
+    "Preconfigured `helm' to browse the prompts of the current Eshell."
     (interactive)
     (helm :sources
           (helm-build-sync-source "Eshell prompts"
@@ -24,4 +60,15 @@
             :action '(("Go to prompt" . helm-eshell-goto-prompt)))
           :buffer "*helm-eshell-prompts*"))
 
+  (defun helm-eshell-browse-all-prompts ()
+    "Preconfigured `helm' to browse the prompts of all Eshell sessions."
+    (interactive)
+    (helm :sources
+          (helm-build-sync-source "Eshell all prompts"
+            :candidates (helm-eshell-all-prompt-list)
+            :action '(("Go to prompt" . helm-eshell-goto-all-prompt)))
+          :buffer "*helm-eshell-all-prompts*"))
+
   (add-to-list 'helm-source-names-using-follow "helm-eshell-prompts"))
+
+(provide 'package-helm-eshell)
