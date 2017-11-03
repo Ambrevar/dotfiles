f2e5e0d1fb6ab6a7d239f7b6362a8661cd206411
Pierre Neidhardt <ambrevar@gmail.com>
2017-11-03 12:02:10 +0100

    Helm/EXWM: Use filters instead of exwm-class-name

--- .emacs.d/lisp/package-helm-exwm.el
+++ .emacs.d/lisp/package-helm-exwm.el
@@ -54,35 +54,6 @@
 ;; A workaround would be to discard the result of kill-buffer and print the
 ;; count manually.
 
-(defvar helm-exwm-map
-  (let ((map (make-sparse-keymap)))
-    (set-keymap-parent map helm-map)
-    (define-key map (kbd "C-c o")   'helm-buffer-switch-other-window)
-    (define-key map (kbd "C-c C-o") 'helm-buffer-switch-other-frame)
-    (define-key map (kbd "M-D")     'helm-buffer-run-kill-buffers)
-    ;; (define-key map (kbd "C-c d")   'helm-buffer-run-kill-persistent)
-    ;; (define-key map (kbd "C-c d")   'helm-exwm-buffer-run-kill-persistent)
-    (define-key map (kbd "C-]")     'helm-exwm-toggle-buffers-details)
-    map)
-  "Keymap for browser source in Helm.")
-
-(defun helm-exwm-candidates (&optional class)
-  "Return the list of EXWM buffers belonging to CLASS.
-
-If CLASS is nil, then list all EXWM buffers."
-  (let ((bufs (delq nil (mapcar
-                         (lambda (buf)
-                           (if (with-current-buffer buf
-                                 (and (eq major-mode 'exwm-mode)
-                                      (or (not class) (string= (downcase exwm-class-name) class))))
-                               (buffer-name buf)
-                             nil))
-                         (buffer-list)))))
-    (when (> (length bufs) 1)
-      ;; Move first buffer (current) to last position.
-      (setcdr (last bufs) (list (pop bufs))))
-    bufs))
-
 (defvar helm-exwm-buffer-max-length 51
   "Max length of EXWM buffer names before truncating.
 When disabled (nil) use the longest buffer-name length found.
@@ -116,38 +87,73 @@ Should be called after others transformers i.e (boring buffers)."
                              (funcall helm-fuzzy-matching-highlight-fn name))
                            (get-buffer i)))))
 
-(defun helm-exwm-buffers (&optional class)
-  "Preconfigured `helm' to list EXWM buffers belonging to CLASS.
+(defvar helm-exwm-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map helm-map)
+    (define-key map (kbd "C-c o")   'helm-buffer-switch-other-window)
+    (define-key map (kbd "C-c C-o") 'helm-buffer-switch-other-frame)
+    (define-key map (kbd "M-D")     'helm-buffer-run-kill-buffers)
+    ;; (define-key map (kbd "C-c d")   'helm-buffer-run-kill-persistent)
+    ;; (define-key map (kbd "C-c d")   'helm-exwm-buffer-run-kill-persistent)
+    (define-key map (kbd "C-]")     'helm-exwm-toggle-buffers-details)
+    map)
+  "Keymap for browser source in Helm.")
 
-If CLASS is nil, then list all EXWM buffers."
+(defun helm-exwm-candidates (&optional filter)
+  "Return the list of EXWM buffers allowed by FILTER.
+
+If FILTER is nil, then list all EXWM buffers."
+  (let ((bufs (delq nil (mapcar
+                         (lambda (buf)
+                           (if (with-current-buffer buf
+                                 (and (eq major-mode 'exwm-mode)
+                                      (or (not filter) (funcall filter))))
+                               (buffer-name buf)
+                             nil))
+                         (buffer-list)))))
+    (when (> (length bufs) 1)
+      ;; Move first buffer (current) to last position.
+      (setcdr (last bufs) (list (pop bufs))))
+    bufs))
+
+(defun helm-exwm (&optional filter)
+  "Preconfigured `helm' to list EXWM buffers allowed by FILTER.
+
+FILTER must be a function returning non-nil for allowed buffers,
+nil for disallowed buffers.  FILTER is run in the context of each
+buffer.
+
+If FILTER is nil, then list all EXWM buffers.
+
+Example: List all EXWM buffers but those running XTerm or the URL browser.
+
+  (helm-exwm (function
+              (lambda ()
+                (pcase (downcase exwm-class-name)
+                  (\"XTerm\" nil)
+                  ((file-name-nondirectory browse-url-generic-program) nil)
+                  (_ t)))))"
   (interactive)
   (helm :sources
         (helm-build-sync-source "EXWM buffers"
-          :candidates (helm-exwm-candidates class)
+          :candidates (helm-exwm-candidates filter)
           :candidate-transformer 'helm-exwm-highlight-buffers
-          :action '(("Switch to browser buffer(s)" . helm-buffer-switch-buffers)
-                    ("Switch to browser buffer(s) in other window `C-c o'" . helm-buffer-switch-buffers-other-window)
-                    ("Switch to browser buffer in other frame `C-c C-o'" . switch-to-buffer-other-frame)
-                    ("Kill browser buffer(s)" . helm-kill-marked-buffers))
+          :action '(("Switch to buffer(s)" . helm-buffer-switch-buffers)
+                    ("Switch to buffer(s) in other window `C-c o'" . helm-buffer-switch-buffers-other-window)
+                    ("Switch to buffer in other frame `C-c C-o'" . switch-to-buffer-other-frame)
+                    ("Kill buffer(s) `M-D`" . helm-kill-marked-buffers))
           ;; When follow-mode is on, the persistent-action allows for multiple candidate selection.
           :persistent-action 'helm-buffers-list-persistent-action
           :keymap helm-exwm-map)
         :buffer "*helm-exwm*"))
 
-;; TODO: Allow for several classes, and allow for class exclusion.
-;; Or pass lambda taking window as parameter? This is more flexible and allows
-;; for comparing anything.  Lambda return value must be the program name.
-;; (defun helm-exwm-browser-filter (buffer)
-;;    (when (string= (downcase exwm-class-name) (file-name-nondirectory browse-url-generic-program))
-;;      browse-url-generic-program))
+
 (defun helm-exwm-switch (class &optional program other-window)
   "Switch to some EXWM windows belonging to CLASS.
-
 If current window is not showing CLASS, switch to the last open CLASS window.
 If there is none, start PROGRAM.
 
 If PROGRAM is nil, it defaults to CLASS.
-
 With prefix argument or if OTHER-WINDOW is non-nil, open in other window."
   ;; If current window is not in `exwm-mode' we switch to it.  Therefore we must
   ;; also make sure that current window is not a Helm buffer, otherwise calling
@@ -155,34 +161,35 @@ With prefix argument or if OTHER-WINDOW is non-nil, open in other window."
   (unless helm-alive-p
     (setq program (or program class)
           other-window (or other-window current-prefix-arg))
-    (if (and (eq major-mode 'exwm-mode)
-             (string= (downcase exwm-class-name) class))
-        (if (fboundp 'helm-exwm-buffers)
-            (helm-exwm-buffers class)
-          (when other-window (other-window 1))
-          (start-process-shell-command program nil program))
-      (let ((last (buffer-list)))
-        (while (and last
-                    (not (with-current-buffer (car last)
-                           (and (eq major-mode 'exwm-mode)
-                                (string= (downcase exwm-class-name) class)))))
-          (setq last (cdr last)))
-        (if last
-            (funcall (if other-window 'switch-to-buffer-other-window 'switch-to-buffer) (car last))
-          (when other-window (select-window (split-window-sensibly)))
-          (start-process-shell-command program nil program))))))
+    (let ((filter (lambda ()
+                    (string= (downcase exwm-class-name) class))))
+      (if (and (eq major-mode 'exwm-mode)
+               (funcall filter))
+          ;; filter: (string= (downcase exwm-class-name) class))
+          (helm-exwm filter)
+        (let ((last (buffer-list)))
+          (while (and last
+                      (not (with-current-buffer (car last)
+                             (and (eq major-mode 'exwm-mode)
+                                  (funcall filter)))))
+            ;; (string= (downcase exwm-class-name) class)))))
+            (setq last (cdr last)))
+          (if last
+              (funcall (if other-window 'switch-to-buffer-other-window 'switch-to-buffer) (car last))
+            (when other-window (select-window (split-window-sensibly)))
+            (start-process-shell-command program nil program)))))))
 
 (defun helm-exwm-switch-browser ()
   "Switch to some `browse-url-generic-program' windows.
 
-See `helm-exwm-start'."
+See `helm-exwm-switch'."
   (interactive)
   (helm-exwm-switch (file-name-nondirectory browse-url-generic-program) browse-url-generic-program))
 
 (defun helm-exwm-switch-browser-other-window ()
   "Switch to some `browse-url-generic-program' windows in other window.
 
-See `helm-exwm-start'."
+See `helm-exwm-switch'."
   (interactive)
   (helm-exwm-switch (file-name-nondirectory browse-url-generic-program) browse-url-generic-program t))
 
