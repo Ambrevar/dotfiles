c994611339f31abf0dffb311fb6b65c55b5a9b94
Pierre Neidhardt <ambrevar@gmail.com>
2017-11-22 15:34:08 +0100

    Helm-EXWM: Add to selected packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -359,7 +359,7 @@
   (set-face-attribute 'wgrep-face nil :inherit 'ediff-current-diff-C :foreground 'unspecified :background 'unspecified :box nil))
 
 ;;; Window manager
-(nconc package-selected-packages '(exwm))
+(nconc package-selected-packages '(exwm helm-exwm))
 (when (require 'exwm nil t) (require 'init-exwm))
 
 ;;; XML / SGML
