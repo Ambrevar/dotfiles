78c2c95b0e570d821e5715ceaede2bedf54c631b
Pierre Neidhardt <ambrevar@gmail.com>
2017-12-23 10:08:55 +0100

    qutebrowser: Start transmission-daemon on demand

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -46,8 +46,8 @@ c.url.searchengines = {
 }
 
 config.bind(',v', 'spawn mpv {url}')
-config.bind(',t', 'hint links spawn transmission-remote -a {hint-url}')
-config.bind(',T', 'hint -r links spawn transmission-remote -a {hint-url}')
+config.bind(',t', 'hint links spawn transmission-auto -a {hint-url}')
+config.bind(',T', 'hint -r links spawn transmission-auto -a {hint-url}')
 config.bind('yy', 'yank -s')
 config.bind('yY', 'yank')
 config.bind('yd', 'yank -s domain')
