e3fb9f40a8fe83b2a45a63ab85b824764fd30331
Pierre Neidhardt <ambrevar@gmail.com>
2018-01-17 10:11:06 +0100

    Evil: Replace contextual HJKL with Evil-Org's shift theme

--- .emacs.d/lisp/init-evil-org.el
+++ .emacs.d/lisp/init-evil-org.el
@@ -5,46 +5,15 @@
 
 (add-hook 'org-mode-hook 'evil-org-mode)
 ;; No need for 'insert, 'shift (I use custom definitions), 'todo 'heading.
-(evil-org-set-key-theme '(navigation textobjects additional))
+(evil-org-set-key-theme '(navigation textobjects additional shift))
 
-;;; This won't get merged upstream.  See
-;;; https://github.com/Somelauw/evil-org-mode/issues/25.
 (defun evil-org-meta-return ()
   "Like `org-meta-return' but switch to insert mode."
   (interactive)
   (evil-insert 1)
   (org-meta-return))
 
-(defun evil-org-shiftleft ()
-  (interactive)
-  ;; TODO: Use `org-at-heading-or-item-p' instead?
-  (if (or (org-on-heading-p) (org-at-planning-p))
-      (org-shiftleft)
-    (evil-window-top)))
-
-(defun evil-org-shiftright ()
-  (interactive)
-  (if (or (org-on-heading-p) (org-at-planning-p))
-      (org-shiftright)
-    (evil-window-bottom)))
-
-(defun evil-org-shiftup ()
-  (interactive)
-  (if (or (org-on-heading-p) (org-at-planning-p))
-      (org-shiftup)
-    (evil-lookup)))
-
-(defun evil-org-shiftdown ()
-  (interactive)
-  (if (or (org-on-heading-p) (org-at-planning-p))
-      (org-shiftdown)
-    (call-interactively 'evil-join)))
-
 (evil-define-key 'normal evil-org-mode-map
-  "H" 'evil-org-shiftleft
-  "J" 'evil-org-shiftdown
-  "K" 'evil-org-shiftup
-  "L" 'evil-org-shiftright
   "^" 'org-up-element ; Evil-Magit-inspired. TODO: Suggest upstream.
   "<" 'org-up-element ; Custom
   ">" 'org-down-element ; Custom
